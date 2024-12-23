9e127b96411b3ebfe471b07dcb4b92b19ea71dd9
unknown <sndnamaku@gmail.com>
2024-12-23 17:57:00 +0700

    apa yaaa

---
+++ if-else.js
@@ -0,0 +1,9 @@
+let nilai = 45;
+
+if (nilai >= 75 ) {
+    console.log ("selamat kamu lulus");
+} else if (nilai <= 30) {
+    console.log ('dasar tol*l');
+} else {
+    console.log ('yah selamat mencoba lagi');
+}
