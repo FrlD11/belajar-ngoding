8de99884915096a491a6ad8975142bca920da227
unknown <sndnamaku@gmail.com>
2024-12-23 17:28:47 +0700

    upload

---
+++ dasar_1.js
@@ -0,0 +1,35 @@
+
+
+let angka1 = 10;
+let angka2 = 20;
+
+console.log ( angka1 + angka2 )
+console.log ( angka1 - angka2 )
+console.log ( angka1 * angka2 )
+console.log ( angka1 / angka2 )
+console.log ( angka1 % angka2 )
+
+let hasilSemua = angka1 + angka2;
+console.log (hasilSemua);
+
+hasilSemua = angka1 - angka2;
+console.log (hasilSemua);
+
+hasilSemua = angka1 * angka2;
+console.log (hasilSemua);
+
+hasilSemua = angka1 / angka2;
+console.log (hasilSemua);
+
+hasilSemua = angka1 + angka2;
+console.log (hasilSemua);
+
+
+let firstName = 'farrel';
+let lastName = 'Defianto';
+let fullName = firstName + ' ' + lastName ;
+
+console.log ( fullName + angka1);
+console.log (firstName + ' ' + "itu" + ' ' + lastName + ' ' + 'wkwkwwk');
+console.log (angka1 + true );
+
---
+++ helloWorld.js
@@ -0,0 +1,25 @@
+
+
+function cekGenapGanjil(angka) {
+    if (angka % 2 === 0) {
+        return "Genap";
+    } else {
+        return "Ganjil";
+    }
+}
+
+function besarKecil(jumlah){
+    if (jumlah > 10 ){
+        return "kecil"
+    } else {
+        return "besar"
+    }
+}
+
+// Contoh penggunaan:
+console.log(cekGenapGanjil(5));  // Output: "Genap"
+console.log(cekGenapGanjil(182));  // Output: "Ganjil"
+// wkwkwkwkwkw
+console.log("kau nyebelin")
+
+console.log (besarKecil(15));
