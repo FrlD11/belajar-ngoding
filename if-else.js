45b20c7b0fb076ed3781f22218a9da7ffa2506c3
unknown <sndnamaku@gmail.com>
2024-12-23 18:56:54 +0700

    tambah lagi

--- if-else.js
+++ if-else.js
@@ -1,10 +1,22 @@
-let nilai = 45;
+let nilai = 1;
 
-if (nilai >= 75 ) {
-    console.log ("selamat kamu lulus");
-} else if (nilai <= 30) {
-    console.log ('dasar tol*l');
+if (nilai >= 90 ) {
+    nilai = "A" ;
+} else if (nilai <= 30 && nilai >1 ) {
+    console.log ('urgh');
+}else if (nilai <= 1) {
+    nilai = 'parah banget';
 } else {
     console.log ('yah selamat mencoba lagi');
 } 
-// wkwkwkwkwkwkk
+console.log (nilai)
+nilai = 100;
+ if (nilai > 99 || nilai <= 1 ){
+    console.log ("mengejutkan")
+ } else if (nilai <50 ){
+    console.log ("wkwkwk")
+ } else {
+    console.log ('hmmmmm')
+ }
+
+    
